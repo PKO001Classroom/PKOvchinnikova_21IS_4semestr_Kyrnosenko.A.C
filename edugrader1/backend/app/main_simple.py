2b99146c14309ae7ea27280e8bebc0db75ace3f9
sakuracreator <905791ilya@mail.ru>
2026-02-19 12:24:43 +0300

    mijg

--- edugrader1/backend/app/main_simple.py
+++ edugrader1/backend/app/main_simple.py
@@ -330,7 +330,7 @@ app = FastAPI(title="EduGrader")
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
